74380ffae3ce1b40fa77951f7c2a1a1a3a7fa744
Corbyn <corbynlangley@gmail.com>
2019-10-27 14:10:25 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import deku from './images/deku.png'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Header(){
+    return <div className="Header" ><p>Welcome to my humble domain</p></div>;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Content(props){
+    let imgClass = 'ContentImage';
+    let textClass;
+    let borderSide;
+    if (props.i%2===0){
+        imgClass += ' ContentImageLeft';
+        textClass = 'ContentTextLeft';
+        borderSide = ' topContent';
+    } else {
+        imgClass += ' ContentImageRight';
+        textClass = 'ContentTextRight';
+        borderSide = '';
+    }
+
+    const imgContent = <div className = {imgClass}><img src = {deku} alt="deku meme"></img></div>;
+    const textContent = <div className = {textClass}><p>The waifu war is brutal.  You may think that calling out someones waifu
+    is all fun and games, but look at this man.  The pain expressed in his face.  This what all weebs are going through
+    right now, every day.</p></div>;
+
+    return <div className= {'Content' + borderSide}>
+        {imgContent}
+        {textContent}
+        </div>;
+}
+
+function IWasHereForm(){
+    return <div className="IWasHereForm">
+        <form>
+        <label>FirstName:</label>
+        <input type="text" name="firstName"></input>
+        <label>LastName:</label>
+        <input type="text" name="firstName"></input>
+        </form>
+        <button>owo</button>
+    </div>;
+}
+
+class App extends React.Component{
+
+     render() {
+        let i = 0;
+
+        return <div className = "App">
+        <Header></Header>
+        <Content i = {i++}></Content>
+        <Content>i = {i++}</Content>
+        <IWasHereForm></IWasHereForm>
+        <Content i = {i++}></Content>
+        <Content>i = {i++}</Content>
+        </div>;
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+    );
