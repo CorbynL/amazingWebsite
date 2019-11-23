34cb6d6e87b3df16a2f896267bdac1d2550fd036
CorbynL <corbynlangley@gmail.com>
2019-11-23 23:28:26 +0200

    Added react-router and Login,Signup stump pages

--- src/index.js
+++ src/index.js
@@ -1,109 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import deku from './images/deku.png'
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import LoginPage from './Login';
+import FrontPage from './main';
+import SignUp from './SignUp';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
 
 
-function Header(){
-    return <div className="Header" ><p>Welcome to my humble domain</p></div>;
-}
+function Page() {
+    // const [data,setData] = useState("Loading...");
 
-function SideBar() {
-    return <div SideBar className="SideBar">
-        <a href="#Home">Home</a>
-        <a href="#Stuff">Stuff</a>
-        <a href="#Cooler Stuff">Cooler Stuff</a>
-
-    </div>;
-}
-
-function NavBar(){
-    return <div className="NavBar">
-        <ul>
-            <li href="#"><a class="active" href="#home">Home</a></li> 
-            <li href="#"><a class="active" href="#home">Home</a></li>
-            <li href="#"><a class="active" href="#home">Home</a></li>
-        </ul>
-    </div>;
-}
-
-function Content({i}){
-    let imgClass = 'ContentImage';
-    let textClass = 'ContentText';
-    let contentPosition = '';
-
-    const imgContent = <div className = {imgClass}><img src = {deku} alt="deku meme"></img></div>;
-    const textContent = <div className = {textClass}>The waifu war is brutal.  You may think that calling out someones waifu
-    is all fun and games, but look at this man.  The pain expressed on his face.  This is what all weebs are going through
-    right now, every day.</div>;
-
-    return <div className= {'Content' + contentPosition}>
-       {i%2===0 ?
-        [imgContent,
-        textContent] :
-        [textContent,
-        imgContent]}
-        </div>;
-}
-
-function Main() {
-    let i = 0;
-
-    const [data,setData] = useState("Loading...");
-
-    useEffect(() => {
-        async function fetchData() {
-           const response = await fetch('http://localhost:3001/');
-           const data = await response.json(); 
-        setData(data.msg);
-        }
-        fetchData();
-   });
+//    useEffect(() => {
+//        async function fetchRandom() {
+//           let response = await fetch('http://localhost:3001/random');
+//           let data = await response.json(); 
+//        setData(data.msg);
+//        }
+//        fetchRandom();
+//   },[data]);
 
     return (
-        <div className="Main">
-        <h1>{data}</h1>
-        <Header />
-        <IWasHereForm setData={setData}></IWasHereForm>
-        <Content i = {i++}></Content>
-        <Content>i = {i++}</Content>
-        <Content i = {i++}></Content>
-        <Content>i = {i++}</Content>
-        </div>);
-}
+    <Router history={createBrowserHistory()}>
+        <div className='Page'>
+            <Route exact path ='/' component={FrontPage}></Route> 
+            <Route path='/Login' component={LoginPage}></Route>
+            <Route path='/SignUp' component={SignUp}></Route>
 
-function IWasHereForm({setData}){
-
-    const handleSubmit = () => {
-        const name = document.getElementById('firstName');
-        setData(name.value);
-    }
-
-    return <div className="IWasHereForm">
-        <form method="post">
-        <label>FirstName:</label>
-        <input type="text" id="firstName"></input>
-        <label>LastName:</label>
-        <input type="text" name="firstName"></input>
-        {/* <break></break> */}
-        </form>  
-        <button type="submit" onClick={handleSubmit}>owo</button>  
-    </div>
-}
-
-class App extends React.Component{
-
-     render() {
-
-        return <div className = "App">
-        <NavBar />
-        <Main />      
-        </div>;
-    }
+        </div>
+    </Router>
+    );
 }
 
 ReactDOM.render(
-    <App />,
+    <Page />,
     document.getElementById('root')
     );
---
+++ src/main.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import './index.css';
+import deku from './images/deku.png'
+import {Link} from 'react-router-dom'
+function Header(){
+    return <div className="Header" ><p>Welcome to my humble domain</p></div>;
+}
+
+function NavBar(){
+    return <div className="NavBar">
+        <ul>
+            <li href="#"><a class="active" href="#home">Home</a></li> 
+            <li href="#"><a class="active" href="#home">Home</a></li>
+            <li href="#"><a class="active" href="#home">Home</a></li>
+        </ul>
+    </div>;
+}
+
+function Content({i}){
+    let imgClass = 'ContentImage';
+    let textClass = 'ContentText';
+    let contentPosition = '';
+
+    const imgContent = <div className = {imgClass}><img src = {deku} alt="deku meme"></img></div>;
+    const textContent = <div className = {textClass}>The waifu war is brutal.  You may think that calling out someones waifu
+    is all fun and games, but look at this man.  The pain expressed on his face.  This is what all weebs are going through
+    right now, every day.</div>;
+
+    return <div className= {'Content' + contentPosition}>
+       {i%2===0 ?
+        [imgContent,
+        textContent] :
+        [textContent,
+        imgContent]}
+        </div>;
+}
+
+function SignUpButton ({setPage}) {
+
+
+    return <div className='SignUpButton'>
+        <Link to ='/SignUp'>
+            <button >Sign Up</button>
+        </Link>
+    </div>
+}
+
+function Main({setPage, data, setData}) {
+    let i = 0;
+
+    return (
+        <div className="Main">
+        <h1>{data}</h1>
+        <Header />
+        <SignUpButton setPage = {setPage}/>
+        <IWasHereForm setData={setData}></IWasHereForm>
+        <Content i = {i++}></Content>
+        <Content>i = {i++}</Content>
+        <Content i = {i++}></Content>
+        <Content>i = {i++}</Content>
+        </div>);
+}
+
+function IWasHereForm({setData}){
+
+    // const handleSubmit = () => {
+    //     const name = document.getElementById('firstName');
+    //     setData(name.value);
+    // }
+
+    return <div className="IWasHereForm">
+        <form method="post">
+        <label>FirstName:</label>
+        <input type="text" id="firstName"></input>
+        <label>LastName:</label>
+        <input type="text" name="firstName"></input>
+        {/* <break></break> */}
+        </form>  
+        <Link to='/Login'>
+            <button type="submit" >owo</button>
+        </Link>  
+    </div>
+}
+
+function FrontPage (props){
+     return (
+        <div className = "FrontPage">
+        <NavBar />
+        <Main {...props} />      
+        </div>
+     );
+}
+
+export default FrontPage;
